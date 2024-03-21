a9974ec851750a5e086866d91b379ea3c3cf52e5
pdamianik <39028343+pdamianik@users.noreply.github.com>
2024-03-22 00:30:03 +0100

    Show product lists
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,11 +1,35 @@
 <script lang="ts">
-	import { Heading } from 'flowbite-svelte';
+	import { Heading, ListPlaceholder, Alert, P } from 'flowbite-svelte';
+	import type { PageData } from './$types';
+	import { type ExistingDocument, type ProductList as PL } from '$lib/db';
+	import ProductList from '$lib/components/ProductList.svelte';
+
+	export let data: PageData;
+
+	export let lists: ExistingDocument<PL>[] = [];
+
+	data.lists.then((result) => {
+		lists = result.docs;
+		return result;
+	});
 </script>
 
 <svelte:head>
 	<title>G'schäft'lhaberer</title>
 </svelte:head>
 
-<div class="text-center">
-	<Heading>G'schäft'l<wbr />Haberer</Heading>
+<Heading class="my-5 text-center">G'schäft'l<wbr />Haberer</Heading>
+
+<div class="mb-5 mt-12 flex flex-col items-center">
+	{#await data.lists}
+		<ListPlaceholder class="w-full max-w-sm" />
+	{:then}
+		{#each lists as list}
+			<ProductList productlist={list} class="my-2" />
+		{:else}
+			<P>Keine Listen vorhanden</P>
+		{/each}
+	{:catch error}
+		<Alert color="red">Ein Fehler ist aufgetreten: {error.message}</Alert>
+	{/await}
 </div>
