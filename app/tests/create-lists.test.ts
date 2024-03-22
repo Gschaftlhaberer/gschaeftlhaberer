969eadc18fe724639bba072ac5b1ef67d7e4ab30
jdoppelhofer <jdoppelhofer@student.tgm.ac.at>
2024-03-22 10:18:07 +0100

    Fix tests

--- app/tests/create-lists.test.ts
+++ app/tests/create-lists.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('createlist page has expected h1', async ({ page }) => {
 	await page.goto('/createlist');
-	await expect(page.getByRole('heading', { name: 'Neue Liste erstellen' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Neue Einkaufsliste erstellen' })).toBeVisible();
 });
 
 test('createlist page has expected input', async ({ page }) => {
