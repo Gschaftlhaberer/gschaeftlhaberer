b71680f815263901142f06007683a8892e3ad0ef
jdoppelhofer <jdoppelhofer@student.tgm.ac.at>
2024-03-22 10:14:52 +0100

    Implement tests

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,13 @@ test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: "G'schäft'lhaberer" })).toBeVisible();
 });
+
+test('createlist page has expected h1', async ({ page }) => {
+	await page.goto('/createlist');
+	await expect(page.getByRole('heading', { name: 'Neue Liste erstellen' })).toBeVisible();
+});
+
+test('createlist page has expected input', async ({ page }) => {
+	await page.goto('/createlist');
+	await expect(page.getByRole('textbox', { name: 'Name' })).toBeVisible();
+});
