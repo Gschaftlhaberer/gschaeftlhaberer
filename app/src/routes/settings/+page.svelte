05901a86889213ab45452591afbf42e0ea266686
jproksch <jproksch@student.tgm.ac.at>
2024-03-22 10:12:25 +0100

    Synch PouchDB to remote CouchDB

--- app/src/routes/settings/+page.svelte
+++ app/src/routes/settings/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import { Input, Button, Label } from 'flowbite-svelte';
+    import PouchDB from 'pouchdb';
     let link: string = '';
     let isValidLink: boolean = false;
+    let replicationError: string | null = null;
 
     function validateLink(value: string): boolean {
         value = value.trim();
@@ -9,16 +11,27 @@
         return pattern.test(value);
     }
     $: isValidLink = link !== '' && validateLink(link);
+
+    async function handleReplication() {
+        if (isValidLink) {
+            try {
+                await PouchDB.replicate('shopping', link);
+                alert("Replikation erfolgreich!");
+            } catch (error) {
+                console.error("Replikationsfehler: ", error);
+                replicationError = "Fehler bei der Replikation. Bitte überprüfen Sie den Link und versuchen Sie es erneut.";
+            }
+        }
+    }
 </script>
 
 <svelte:head>
     <title>Settings - G'schäft'lhaberer</title>
 </svelte:head>
 
-
-<form on:submit|preventDefault>
+<form on:submit|preventDefault={handleReplication}>
     <div class="flex flex-col gap-4">
-		<Label for="first_name" class="mb-2">First name</Label>
+        <Label for="first_name" class="mb-2">First name</Label>
         <Input
             bind:value={link}
             on:input={() => isValidLink = validateLink(link)}
@@ -28,8 +41,11 @@
             class={isValidLink ? 'valid-input' : link !== '' ? 'invalid-input' : ''}
         />
 
-        <Button type="submit" disabled={!isValidLink} class={isValidLink ? 'valid-button' : ''}>
+        <Button color="green" type="submit" disabled={!isValidLink} class={isValidLink ? 'valid-button' : ''}>
             Bestätigen
         </Button>
+        {#if replicationError}
+            <p class="text-red-500">{replicationError}</p>
+        {/if}
     </div>
 </form>
