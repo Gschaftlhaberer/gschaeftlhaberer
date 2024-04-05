5526794066ed4aeff695663bc7d1955541330e2c
pdamianik <39028343+pdamianik@users.noreply.github.com>
2024-04-05 10:16:30 +0200

    Fix formatting
    Signed-off-by: pdamianik <39028343+pdamianik@users.noreply.github.com>

--- app/src/lib/components/ListenÜbersicht.svelte
+++ app/src/lib/components/ListenÜbersicht.svelte
@@ -1,19 +1,18 @@
 <script>
-    import { onMount } from 'svelte';
-    import { db } from '$lib/db';
-    import ListenItem from './ListenItem.svelte';
-  
-    let listen = [];
-  
-    onMount(async () => {
-      const result = await db.allDocs({ include_docs: true, descending: true });
-      listen = result.rows.map(row => row.doc);
-    });
-  </script>
-  
-  <div>
-    {#each listen as liste}
-      <ListenItem {liste} />
-    {/each}
-  </div>
-  
+	import { onMount } from 'svelte';
+	import { db } from '$lib/db';
+	import ListenItem from './ListenItem.svelte';
+
+	let listen = [];
+
+	onMount(async () => {
+		const result = await db.allDocs({ include_docs: true, descending: true });
+		listen = result.rows.map((row) => row.doc);
+	});
+</script>
+
+<div>
+	{#each listen as liste}
+		<ListenItem {liste} />
+	{/each}
+</div>
